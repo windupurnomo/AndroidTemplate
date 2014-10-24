be907291b3be7a46c3227cd36d460624582d9ea4
windupurnomo <wpwindupurnomo@gmail.com>
2014-10-25 03:38:12 +0700

    Slide Menu. Splash - Login - Slide Menu

--- app/src/main/java/com/windupurnomo/kambingindonesia/activities/MainMenu.java
+++ app/src/main/java/com/windupurnomo/kambingindonesia/activities/MainMenu.java
@@ -1,34 +1,35 @@
 package com.windupurnomo.kambingindonesia.activities;
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.windupurnomo.kambingindonesia.R;
 
-public class Login extends Activity {
+public class MainMenu extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        Button btnSignIn = (Button) findViewById(R.id.btnSignIn);
-        btnSignIn.setOnClickListener(signInOC);
+        setContentView(R.layout.activity_main_menu);
+
+        ImageView ivProfile = (ImageView)findViewById(R.id.ivProfile);
+        TextView tvProfile = (TextView)findViewById(R.id.tvProfile);
+        ivProfile.setOnClickListener(clickProfile);
+        tvProfile.setOnClickListener(clickProfile);
+
+
     }
 
-    private View.OnClickListener signInOC = new View.OnClickListener(){
+    private View.OnClickListener clickProfile = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent i = new Intent(Login.this, MainActivity.class);
-            startActivity(i);
+
         }
     };
-
-
 }
